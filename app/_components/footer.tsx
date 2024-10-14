2eef9911c1c21cd0bb778a5a58af9ccaa5ebcd86
Pedro Faleiros <pedro.alves.faleiros@gmail.com>
2024-10-13 21:34:32 -0300

    feat: add initial Footer component

---
+++ app/_components/footer.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+const Footer = () => {
+  return (
+    <footer className="bg-primary px-6 py-4">
+      <Image src="/logo-menu.png" alt="Samph" width={100} height={100} />
+    </footer>
+  );
+};
+
+export default Footer;
