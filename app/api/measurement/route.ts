704397599334d1b3dfee6b4f0637264923ba6df7
Pedro Faleiros <pedro.alves.faleiros@gmail.com>
2024-11-17 21:39:20 -0300

    feat: add measurement api

---
+++ app/api/measurement/route.ts
@@ -0,0 +1,38 @@
+import { db } from "@/app/_lib/db";
+import { revalidatePath } from "next/cache";
+import { NextResponse } from "next/server";
+import { z } from "zod";
+
+const measurementSchema = z.object({
+  poolId: z.string().min(1, "Id da piscina é obrigatório."),
+  phValue: z.string().min(1, "Valor do pH é obrigatório."),
+});
+
+export async function POST(req: Request) {
+  const body = await req.json();
+  const { poolId, phValue } = measurementSchema.parse(body);
+
+  try {
+    const date = new Date();
+
+    await db.measurement.create({
+      data: {
+        poolId,
+        phValue: Number(phValue),
+        date,
+      },
+    });
+
+    revalidatePath(`/pools/${poolId}`);
+
+    return NextResponse.json(
+      { message: "Medição cadastrada com sucesso!" },
+      { status: 201 },
+    );
+  } catch (error) {
+    return NextResponse.json(
+      { message: "Algo deu errado.", error: error },
+      { status: 500 },
+    );
+  }
+}
